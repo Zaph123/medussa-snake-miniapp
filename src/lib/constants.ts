24108ab839d7d2ec8ada07684322ffccddb249da
Zaphenath Bassey <bassey2108@gmail.com>
2025-12-09 09:19:59 +0100

    installed @farcaster/miniapp-core package

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
-import { type AccountAssociation } from '@farcaster/miniapp-core/src/manifest';
+import type { AccountAssociation } from '@farcaster/miniapp-core/src/manifest';
 
 /**
  * Application constants and configuration values.
